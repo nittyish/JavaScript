5b47d4180d9117ec30d2bf7d5b019ba53d964cfa
Nitish Sharma <www.sharmanitish.sudha@gmail.com>
2024-04-09 16:52:31 +0000

    Variable and datatypes

---
+++ 01-Basics/01-variable.js
@@ -0,0 +1,12 @@
+const accountId = 1445566
+let accountEmail = "nitish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Saharanpur"
+let accountState;
+
+accountState = "Uttar Pradesh"
+
+accountEmail = "nitish@phonepay.com"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
